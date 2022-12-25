ca1a891111f1562766558ce8fa737033e0fa108e
fszf <frankshin82@gmail.com>
2022-12-25 01:14:01 -0800

    updates

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -8,16 +8,16 @@ const sumfetch = async (args: string[]): Promise<string> => {
       @@                       @@            ABOUT
     @@                           @@          f604
   @@                               @@       ﰩ ${config.ps1_hostname}
- @@                         @@@     @@       <u><a href="${config.blog_url}" target="_blank">Blog</a></u>
-@@        @@@                        @@     
+ @@                         @@@     @@       <u><a href="${config.blog_url}" target="_blank">read my blog</a></u>
+@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u> 
 @@                                   @@     -----------
 @@             .@@@@@@@@@@.          @@      CONTACT 
  @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">Github</a></u>
-   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">LinkedIN</a></u>
+  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github</a></u>
+   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin</a></u>
      @@@                        @@@         -----------
         @@@                  @@@ @@          DONATE 
-         @|  @@@@@@@@@@@@@@@@   @@           <u><a href="${config.donate_urls.paypal}" target="_blank">Send me money via PayPal</a></u>
+         @|  @@@@@@@@@@@@@@@@   @@           <u><a href="${config.donate_urls.paypal}" target="_blank">send me money via paypal</a></u>
          @|                      @@          
 
 `;
